05bccf82c9bb39ea5a079506553d7d8e07c02f25
Andrew Zigler <andrewzigler@gmail.com>
2022-04-24 19:52:35 -0700

    Use new domains

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 git submodule update --init
-sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d healthchecks.andrewzigler.com
-sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d deckofcards.andrewzigler.com
+sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d hc1.andrewzigler.com
+sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d doc1.andrewzigler.com
 
-sudo cp /etc/letsencrypt/live/healthchecks.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.crt
-sudo cp /etc/letsencrypt/live/healthchecks.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/hc1.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.andrewzigler.com.crt
+sudo cp /etc/letsencrypt/live/hc1.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.andrewzigler.com.key
 
-sudo cp /etc/letsencrypt/live/deckofcards.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.crt
-sudo cp /etc/letsencrypt/live/deckofcards.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/doc1.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.andrewzigler.com.crt
+sudo cp /etc/letsencrypt/live/doc1.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.andrewzigler.com.key
