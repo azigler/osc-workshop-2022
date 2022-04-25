4cb1cac448d8149da446874dadb230d62e147564
Andrew Zigler <andrewzigler@gmail.com>
2022-04-24 19:46:07 -0700

    Copy nginx files into container

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ git submodule update --init
 sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d healthchecks.andrewzigler.com
 sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d deckofcards.andrewzigler.com
 
-sudo ln -s /etc/letsencrypt/live/healthchecks.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.crt
-sudo ln -s /etc/letsencrypt/live/healthchecks.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/healthchecks.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.crt
+sudo cp /etc/letsencrypt/live/healthchecks.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/healthchecks.andrewzigler.com.key
 
-sudo ln -s /etc/letsencrypt/live/cards.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.crt
-sudo ln -s /etc/letsencrypt/live/cards.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/deckofcards.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.crt
+sudo cp /etc/letsencrypt/live/deckofcards.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/deckofcards.andrewzigler.com.key
