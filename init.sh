7df1fb56e4361f216594789403e2eebc8d10273f
Andrew Zigler <andrewzigler@gmail.com>
2022-05-03 11:32:06 -0700

    Change init script to take URL argument

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+[ -z "$1" ] && echo "Please provide a domain name (e.g., example.com)" && exit 1
+
 git submodule update --init
-sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d hc1.andrewzigler.com
-sudo certbot certonly --non-interactive --standalone --agree-tos -m me@andrewzigler.com -d doc1.andrewzigler.com
+sudo certbot certonly --non-interactive --standalone --agree-tos -m info@$1 -d hc1.$1
+sudo certbot certonly --non-interactive --standalone --agree-tos -m info@$1 -d doc1.$1
 
-sudo cp /etc/letsencrypt/live/hc1.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.andrewzigler.com.crt
-sudo cp /etc/letsencrypt/live/hc1.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/hc1.$1/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.$1.crt
+sudo cp /etc/letsencrypt/live/hc1.$1/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.$1.key
 
-sudo cp /etc/letsencrypt/live/doc1.andrewzigler.com/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.andrewzigler.com.crt
-sudo cp /etc/letsencrypt/live/doc1.andrewzigler.com/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.andrewzigler.com.key
+sudo cp /etc/letsencrypt/live/doc1.$1/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.$1.crt
+sudo cp /etc/letsencrypt/live/doc1.$1/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.$1.key
