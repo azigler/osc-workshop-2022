6a242d2fa8a839bc0884b02fa874a60c617671d7
Andrew Zigler <andrewzigler@gmail.com>
2022-05-03 15:44:59 -0700

    Update docker-compose
    - improve init script
    - gitkeep directories

--- init.sh
+++ init.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-[ -z "$1" ] && echo "Please provide a domain name (e.g., example.com)" && exit 1
+if [ `arch` != "arm64" ]
+then
+    sudo chown -R 2000:2000 ./mattermost/app/mattermost
+fi
 
-git submodule update --init
-sudo certbot certonly --non-interactive --standalone --agree-tos -m info@$1 -d hc1.$1
-sudo certbot certonly --non-interactive --standalone --agree-tos -m info@$1 -d doc1.$1
-
-sudo cp /etc/letsencrypt/live/hc1.$1/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.$1.crt
-sudo cp /etc/letsencrypt/live/hc1.$1/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/hc1.$1.key
-
-sudo cp /etc/letsencrypt/live/doc1.$1/fullchain.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.$1.crt
-sudo cp /etc/letsencrypt/live/doc1.$1/privkey.pem /home/ubuntu/osc-workshop-2022/gitlab/nginx/certs/doc1.$1.key
