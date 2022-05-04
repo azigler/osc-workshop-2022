c2a5ee6eccd50f35eb3ae2ad1fde26ec7fe76f71
Andrew Zigler <andrewzigler@gmail.com>
2022-05-04 12:57:48 -0700

    Use DEMO_HOSTNAME env var

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -z "$HOSTNAME" ] && echo "Please export a hostname to your bash environment (e.g., export HOSTNAME=example.com)" && exit 1
+[ -z "$DEMO_HOSTNAME" ] && echo "Please export a demo hostname to your bash environment (e.g., export DEMO_HOSTNAME=example.com)" && exit 1
 
 git submodule update --init
 sudo certbot certonly --non-interactive --standalone --agree-tos -m info@$1 -d hc1.$1
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -z "$HOSTNAME" ] && echo "Please export a hostname to your bash environment (e.g., export HOSTNAME=example.com)" && exit 1
+[ -z "$DEMO_HOSTNAME" ] && echo "Please export a demo hostname to your bash environment (e.g., export DEMO_HOSTNAME=example.com)" && exit 1
 
 git submodule update --init
 sudo docker-compose up -d --build
